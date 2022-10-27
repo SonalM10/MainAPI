f78ab54a0d376b193ba70c0655e156e5ad1ebec2
Sonal More <sonal.more10@hotmail.com>
2022-10-27 14:59:10 +0530

    Add project files.

---
+++ MainAPI/Model/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainAPI.Model
+{
+    public class State
+    {
+        public int StateId { get; set; }
+        public string Statename { get; set; }
+    }
+}
